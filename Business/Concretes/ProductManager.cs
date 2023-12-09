7d178389da23c726a84b595c6eb8c836282be8bc
PC <burakugras67@gmail.com>
2023-12-09 21:01:47 +0300

    Add project files.

---
+++ Business/Concretes/ProductManager.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using Business.Abstracts;
+using Business.Dtos.Requests;
+using Business.Dtos.Responses;
+using Core.DataAccess.Paging;
+using DataAccess.Abstracts;
+using Entities.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concretes
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+        IMapper _mapper;
+
+        public ProductManager(IProductDal productDal, IMapper mapper)
+        {
+            _productDal = productDal;
+            _mapper = mapper;
+        }
+
+        public async Task<CreatedProductResponse> Add(CreateProductRequest createProductRequest)
+        {
+            Product product = _mapper.Map<Product>(createProductRequest);
+            Product createdProduct = await _productDal.AddAsync(product);
+
+            CreatedProductResponse createdProductResponse = _mapper.Map<CreatedProductResponse>(createdProduct);
+
+            return createdProductResponse;      
+
+        }
+
+        public async Task<IPaginate<GetListProductResponse>> GetListAsync()
+        {
+            var productList = await _productDal.GetListAsync();
+            var mappedList=_mapper.Map<Paginate<GetListProductResponse>>(productList);
+
+            return mappedList;
+
+            
+        }
+    }
+}
---
+++ Business/Dtos/Profiles/ProductMappingProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Business.Dtos.Requests;
+using Business.Dtos.Responses;
+using Core.DataAccess.Paging;
+using Entities.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Dtos.Profiles
+{
+    public class ProductMappingProfile:Profile
+    {
+        public ProductMappingProfile()
+        {
+            CreateMap<CreateProductRequest, Product>();
+            CreateMap<Product,CreatedProductResponse>();
+
+            CreateMap<Product, GetListProductResponse>().ReverseMap();
+            CreateMap<Paginate<Product>,Paginate<GetListProductResponse>>().ReverseMap();
+        }
+    }
+}
